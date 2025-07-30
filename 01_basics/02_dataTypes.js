a1b27114eed7c1c09bbe94c3709c386a01c52d60
ashutosh0203 <161915323+ashutosh0203@users.noreply.github.com>
2025-07-30 13:36:36 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat sll JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // Code readability should be high
+console.log("ASHU")
+
+let name = "AShu"
+let age=18
+let isLoggedIn=false
+
+// number=> 2 to power 53
+//bigint
+// string=>""
+//boolean => true/false
+//null=>standlone value
+// undefined=>
+// symbol
+
+
+//object
+
+console.log(typeof null ); // object
+console.log(typeof undefined ); //undefined
+
